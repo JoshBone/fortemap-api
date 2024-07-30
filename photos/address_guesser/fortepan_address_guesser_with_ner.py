cae236fe883b111d22c39ee970782df20a4614db
Josh <jozsef.bone@gmail.com>
2024-07-30 14:54:38 +0200

    Update fortepan_address_guesser_with_ner.py

--- photos/address_guesser/fortepan_address_guesser_with_ner.py
+++ photos/address_guesser/fortepan_address_guesser_with_ner.py
@@ -1,27 +1,34 @@
-import json
 import re
+import time
 
-import requests
-from django.conf import settings
 from geopy import Nominatim, MapBox
+from transformers import pipeline
 
 PLACES = ['sarok', 'sarkán', 'szemben', 'torkolat', 'torkolatánál', 'torkolata', 'távolban',
           'háttérben', 'hátul', 'balra', 'jobbra', 'felől', 'felé', 'környékén', 'között']
 
-RAGOK = ['nál', 'nél', 'tól', 'től', 'ból', 'ből', 'ban', 'ben', 'on', 'en', 'ön', 'ról', 'ről']
+RAGOK = ['nál', 'nél', 'tól', 'től', 'ból', 'ből', 'ban', 'ben', 'on', 'en', 'én', 'ön', 'ról', 'ről', 'i']
 
-BLACKLIST = ['Budapest', 'Duna']
+BLACKLIST = ['Budapest', 'Duna', 'Győr', 'Rába']
+
+SUPPORTED_TAGS = ['FAC', 'LOC', 'GPE', 'CARDINAL']
 
 class FortepanAddressGuesserWithNER:
-    def __init__(self, description, city, id):
+    def __init__(self, description, city, id, district=None):
+        self.pipeline = pipeline(
+            task="ner",
+            model="novakat/nerkor-cars-onpp-hubert"
+        )
         self.description = self.remove_parentheses(description).replace("  ", " ")
         self.city = city
+        self.district = district
+        self.recognized_parts = []
         self.address_object = {
             'id': id,
             'fortepan_url': 'https://fortepan.hu/hu/photos/?id=%s' % id,
             'fortepan_photo_url': 'https://fortepan.download/file/fortepan-eu/1600/fortepan_%s.jpg' % id,
-            'description': description,
-            'tagged_text': '',
+            'description': self.remove_parentheses(description).replace("  ", " "),
+            'tagged_text': self.remove_parentheses(description).replace("  ", " "),
             'places': []
         }
 
@@ -37,95 +44,127 @@ class FortepanAddressGuesserWithNER:
         return re.sub(r'\([^)]*\)', '', text)
 
     def ner(self):
-        url = "https://juniper.nytud.hu/demo/nlp/ner"
-        r = requests.post(
-            url,
-            json={"data": self.description}
-        )
-        if r.status_code == 200:
-            response = json.loads(r.text)
-            tagged_text = response['ner']
-            self.address_object['tagged_text'] = tagged_text
+        tagged_text = self.pipeline(self.description, aggregation_strategy="simple")
+        for idx, tag in enumerate(tagged_text):
+            finding = {}
+            skip = False
 
-    def geocode_nominatim(self, tagged_text):
-        list = []
-        pattern = r'\[LOC-B\](.*?)\[LOC-E\]'
+            if tag['entity_group'] in SUPPORTED_TAGS:
+
+                # Handle the '##' case
+                if len(self.recognized_parts) > 0:
+                    if self.recognized_parts[-1]['end'] == tag['start'] or tag['word'].startswith("##"):
+                        self.recognized_parts[-1]['text'] += tag['word'].replace('##', '').replace(' - ', '-')
+                        self.recognized_parts[-1]['end'] = tag['end']
+                        skip = True
+
+                # Handle house number
+                if tag['entity_group'] == 'CARDINAL':
+                    if len(self.recognized_parts) > 0:
+                        if self.recognized_parts[-1]['end'] == tag['start'] - 1:
+                            # Check for the - solution
+                            if self.description[int(tag['start']) - 1] == '-':
+                                self.recognized_parts[-1]['text'] += '-%s' % tag['word']
+                            else:
+                                self.recognized_parts[-1]['text'] += ' %s' % tag['word']
+                            self.recognized_parts[-1]['end'] = tag['end']
+                    skip = True
+
+                ## Handle - cases
+                if tag['entity_group'] != 'CARDINAL':
+                    if len(self.recognized_parts) > 0:
+                        if self.recognized_parts[-1]['end'] == tag['start'] - 1:
+                            if self.description[int(tag['start']) - 1] == '-':
+                                self.recognized_parts[-1]['text'] += '-%s' % tag['word']
+                                skip = True
+
+                if not skip:
+                    finding['type'] = tag['entity_group']
+                    finding['text'] = tag['word'].replace(' - ', '-')
+                    finding['start'] = tag['start']
+                    finding['end'] = tag['end']
+                    self.recognized_parts.append(finding)
+
+        # Do the tagging
+        for idx, part in enumerate(self.recognized_parts):
+            # First part of the tags
+            if idx == 0:
+                self.address_object['tagged_text'] = self.description[0:part['start']]
+                self.address_object['tagged_text'] += "[LOC-B]%s[LOC-E]" % part['text']
+
+            # Rest of the stuff
+            if idx > 0 and idx + 1 <= len(self.recognized_parts):
+                self.address_object['tagged_text'] += self.description[self.recognized_parts[idx-1]['end']:part['start']]
+                self.address_object['tagged_text'] += "[LOC-B]%s[LOC-E]" % part['text']
+
+            # Last part of the tags
+            if idx + 1 == len(self.recognized_parts):
+                self.address_object['tagged_text'] += self.description[part['end']:]
+
+            # Add part to the pages array
+            self.address_object['places'].append(part['text'])
+
+    def geocode_nominatim(self, preferred_postal_codes):
         geolocator = Nominatim(user_agent="Fortepan Photo GeoCoding Test")
-        matches = re.findall(pattern, tagged_text)
+        places = []
 
-        for match in matches:
-            if not self.is_between_parentheses(self.description, match):
-                location_object = {}
+        for place in self.address_object['places']:
+            location_object = {}
+            full_address = place.replace('utca', 'u.')
 
-                # Try to get the house number
-                pattern = r' (\d+)'
-                regex_pattern = re.compile(re.escape(match) + pattern)
-                number_group = re.search(regex_pattern, self.description)
-                if number_group:
-                    number = number_group.group(1)
-                    full_address = "%s %s" % (match, number)
-                else:
-                    full_address = match
+            # Ragtalanítás
+            for rag in RAGOK:
+                if full_address[-len(rag):] == rag:
+                    full_address = full_address[:-len(rag)]
 
-                # Changes
-                full_address = full_address.replace('utca', 'u.')
+            if full_address.strip() not in BLACKLIST:
+                # Try to locate
+                try:
+                    if self.district:
+                        query = "%s%s, %s" % (self.city, self.district, full_address)
+                    else:
+                        query = "%s, %s" % (self.city, full_address)
 
-                # Ragtalanítás
-                for rag in RAGOK:
-                    if full_address[-len(rag):] == rag:
-                        full_address = full_address[:-len(rag)]
+                    location = geolocator.geocode(
+                        query=query,
+                        exactly_one=False,
+                        limit=3,
+                        addressdetails=True
+                    )
+                    time.sleep(0.5)
+                except Exception:
+                    print("Geocoder Error")
+                    location = None
 
-                if full_address.strip() not in BLACKLIST:
-                    # Try to locate
-                    address = '%s %s' % (self.city, full_address)
-                    try:
-                        location = geolocator.geocode(address)
-                        if location:
-                            location_object['original_address'] = address
-                            location_object['geocoded_address'] = location.address
-                            location_object['latitude'] = location.latitude
-                            location_object['longitude'] = location.longitude
-                    except Exception:
-                        print("Error locating address: %s" % address)
+                if location:
+                    add = False
+                    for loc in location:
+                        if 'postcode' in loc.raw['address']:
+                            if loc.raw['address']['postcode'] in preferred_postal_codes:
+                                add = True
+                                loc_obj = loc
+                                break
+                        if 'city' in loc.raw['address']:
+                            if loc.raw['address']['city'] == self.city:
+                                add = True
+                                loc_obj = loc
+                                break
+                        if 'town' in loc.raw['address']:
+                            if loc.raw['address']['town'] == self.city:
+                                add = True
+                                loc_obj = loc
+                                break
+                        if 'village' in loc.raw['address']:
+                            if loc.raw['address']['village'] == self.city:
+                                add = True
+                                loc_obj = loc
+                                break
 
-                    list.append(location_object)
-        return list
+                    if add:
+                        location_object['original_address'] = "%s, %s" % (self.city, full_address)
+                        location_object['geocoded_address'] = loc_obj.address
+                        location_object['latitude'] = loc_obj.latitude
+                        location_object['longitude'] = loc_obj.longitude
 
-    def geocode_mapbox(self, tagged_text):
-        lst = []
-        pattern = r'\[LOC-B\](.*?)\[LOC-E\]'
-        geolocator = MapBox(api_key=getattr(settings, 'MAPBOX_API_KEY', None))
-        matches = re.findall(pattern, tagged_text)
-
-        for match in matches:
-            if not self.is_between_parentheses(self.description, match):
-                location_object = {}
-
-                # Try to get the house number
-                pattern = r' (\d+)'
-                regex_pattern = re.compile(re.escape(match) + pattern)
-                number_group = re.search(regex_pattern, self.description)
-                if number_group:
-                    number = number_group.group(1)
-                    full_address = "%s %s" % (match, number)
-                else:
-                    full_address = match
-
-                # Changes
-                full_address = full_address.replace('utca', 'u.')
-
-                if full_address.strip() not in BLACKLIST:
-                    # Try to locate
-                    address = '%s %s' % (self.city, full_address)
-                    try:
-                        location = geolocator.geocode(query=address, exactly_one=True)
-                        if location:
-                            location_object['original_address'] = address
-                            location_object['geocoded_address'] = location.address
-                            location_object['latitude'] = location.latitude
-                            location_object['longitude'] = location.longitude
-                    except Exception:
-                        print("Error locating address: %s" % address)
-
-                    lst.append(location_object)
-        return lst
+            places.append(location_object)
+        return places
