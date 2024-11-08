cc02958c3535e12a7b416890ac120f13e349ee40
Josh <jozsef.bone@gmail.com>
2024-11-08 08:13:50 +0100

    Indexing

---
+++ photos/indexer.py
@@ -0,0 +1,55 @@
+import json
+import os
+
+import meilisearch
+from django.conf import settings
+
+
+class FortepanLocationIndexer:
+    """
+    Class to index Location records.
+    """
+
+    def __init__(self, place='Budapest V.'):
+        FILEZ = {
+            'Budapest V.': 'FortepanVker10000.json',
+            'Győr': 'gyor-all.json'
+        }
+        self.place = place
+        self.data_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'management', 'commands', FILEZ.get(place, ''))
+        self.data = json.load(open(self.data_file))
+        self.fortepan_data = {}
+        self.location = None
+        self.doc = {}
+
+        self.meilisearch_url = getattr(settings, "MEILISEARCH_URL", "")
+        self.meilisearch_api_key = getattr(settings, "MEILISEARCH_API_KEY", "")
+        self.meilisearch_index_name = getattr(settings, "MEILISEARCH_INDEX", "meilisearch")
+
+        self.client = meilisearch.Client(self.meilisearch_url, self.meilisearch_api_key)
+        self.meilisearch_index = self.client.index(self.meilisearch_index_name)
+
+    def get_fortepan_data(self, fortepan_id):
+        self.fortepan_data = list(filter(lambda d: d['_source']['mid'] == [fortepan_id], self.data['hits']['hits']))[0]
+
+    def set_location(self, location):
+        self.location = location
+
+    def make_document(self):
+        self.doc['id'] = self.location.id
+        self.doc['fortepan_id'] = self.location.photo.fortepan_id
+        self.doc['_geo'] = {
+            'lat': self.location.latitude,
+            'lng': self.location.longitude,
+        }
+        self.doc['cimke'] = self.fortepan_data['_source']['cimke_name']
+        self.doc['adomanyozo'] = self.fortepan_data['_source']['adomanyozo_name']
+        self.doc['leiras'] = self.fortepan_data['_source']['description'][0]
+        self.doc['varos'] = self.place
+
+    def index(self):
+        try:
+            self.meilisearch_index.add_documents([self.doc])
+            print("One document added")
+        except Exception as e:
+            print('Error with Report No. %s! Error: %s' % (self.doc['id'], e))
---
+++ photos/management/commands/index_data.py
@@ -0,0 +1,18 @@
+from django.core.management import BaseCommand
+
+from photos.indexer import FortepanLocationIndexer
+from photos.models import Photo, Location
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument("--place", type=str, default='Budapest V.')
+
+    def handle(self, *args, **options):
+        indexer = FortepanLocationIndexer(place=options['place'])
+        for location in Location.objects.filter(photo__status='OK', photo__place=options['place']).all():
+            indexer.get_fortepan_data(location.photo.fortepan_id)
+            indexer.set_location(location)
+            indexer.make_document()
+            indexer.index()
+            pass
